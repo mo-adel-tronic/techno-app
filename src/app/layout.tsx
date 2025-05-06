2eb58b7c17181618d71fabeee282eb6c8b19fd2f
MohamedAbouzaid <mo.adel.developer@gmail.com>
2025-05-06 19:43:24 +0300

    feat(template): add RootRender => RootLayout Component

---
+++ src/app/layout.tsx
@@ -0,0 +1,20 @@
+import RootRender from "@/components/shared/RootLayout";
+import { ImageSrc } from "@/constants/MediaSrc";
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  icons: ImageSrc.LOGO_ICON,
+  title: {
+    default: "Techno Torial Admin",
+    template: "%s | Techno Torial Admin",
+  }
+}
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return <RootRender>
+    {children}
+  </RootRender>
+}
