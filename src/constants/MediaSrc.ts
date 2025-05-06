52e66c511d1acbef5bf245e0b6b9d48995061e1a
MohamedAbouzaid <mo.adel.developer@gmail.com>
2025-05-06 18:41:50 +0300

    feat(media): define image constants and add global UI assets

---
+++ src/constants/MediaSrc.ts
@@ -0,0 +1,7 @@
+export enum ImageSrc {
+    //! Global Images -------------------------------
+    LOGO = '/images/globals/logo.png',
+    LOGO_ICON = '/favicon.ICO',
+    FALLBACK = '/images/globals/fallback.png',
+    BLUR_PLACEHOLDER = '/images/globals/blur-placeholder.png',
+}
